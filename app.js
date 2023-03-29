7ddcda702c18fe2c38ffbfe74062c3af9d5d6968
jungheeeun <jungheeeun272@gmail.com>
2023-03-29 10:55:52 +0900

    class 이해하기

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = new Date(); // 이게 뭘까? 이해하기 . 
+console.dir(a.getFullYear());
+
+
+// const b = {
+//   year: 2023,
+//   month :3,
+//   day : 29
+// }
+// console.log(b.year + '년도');
+// 리터럴 = 직접 넣은 값 
